a8d177cd5b8017e8750fcafa538fd3916c7755fa
LRenault <lrenault@MacMiniPro.local>
2025-06-11 18:55:43 -0400

    Added LittleLemon Swift Project folder

---
+++ LittleLemon/LittleLemonUITests/LittleLemonUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  LittleLemonUITestsLaunchTests.swift
+//  LittleLemonUITests
+//
+//  Created by LRenault on 6/11/25.
+//
+
+import XCTest
+
+final class LittleLemonUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
